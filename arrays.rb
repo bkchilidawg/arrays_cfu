5c3a5b1067b8744105199e8434b1cc878eef235c
Blaine Kennedy <blainekennedy@gmail.com>
2023-07-12 16:13:38 -0500

    Add 4 methods to the arrays

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ friends = ["Bud", "Barry", "Bob", "Brent"]
 age = [20,21,22,23]
 amount_paid = [1.50, 2.50, 3.50, 4.50]
 greater_or_less = [>3,<3,>4,<4]
+
+friends.pop
+# It will remove and call the last string
+
+age.push(24)
+# It will add the integer 24 to the end of the  array
+
+amount_paid.shift
+# It will remove and call the first float
+
+greater_or_less.unshift(>2)
+# It will add >2 in the 0 position
